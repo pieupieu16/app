65e5c5fabbc4314d832aa35fc2371e53eee1e618
Trần Quân <pieupieu>
2025-12-24 07:10:57 +0700

    ml

--- sc.py
+++ sc.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyA93cqzdxbddEYoUWoBZk-aUhfk6X4l5xk")
+genai.configure(api_key="AIzaSyByjCMj8hePosY-SIBOrHQWtL1bVEBGLpQ")
 
 for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
